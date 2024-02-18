21241ab45c99aeb6b4e57ac23b8f07dd6b0386d1
warda <wardahaibe41@gmail.com>
2024-02-18 21:01:25 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const searchicon1= document.querySelector('#searchicon-1');
+const srcicon1= document.querySelector(".srcicon-1");
+const searchinput1= document.querySelector('#searchinput-1');
+
+    searchicon1.addEventListener("click", function(){
+    searchinput1.style.dispalay= 'flex';
+    searchicon1.style.dispalay="none";
+
+})
+
+const searchicon2= document.querySelector('#searchicon-2');
+const srcicon2= document.querySelector(".srcicon");
+const searchinput2= document.querySelector('#searchinput-2');
+
+    searchicon2.addEventListener("click", function(){
+    searchinput2.style.dispalay= 'flex';
+    searchicon2.style.dispalay="none";
+
+})
